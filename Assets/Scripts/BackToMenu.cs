ece971e91da465989a1bfbffd364df6dbd17a8c8
tokudosan12 <16.12t3.doquochuy@gmail.com>
2025-07-05 09:32:44 +0700

    instruct

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    // Hàm gọi khi bấm nút "Back"
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene"); // Đổi tên nếu khác
+    }
+}
